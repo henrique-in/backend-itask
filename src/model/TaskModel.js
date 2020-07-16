10526055a5985cba95c56ee77d6d2f760a6ce5cf
Henrique Oliveira <henrique.fsa97@hotmail.com>
2020-07-16 17:27:47 -0300

    :sparkles: middlewares for validation

--- src/model/TaskModel.js
+++ src/model/TaskModel.js
@@ -5,7 +5,7 @@ const TaskSchema = new Schema({
   macaddress: {type: String, required: true},
   type: {type: Number, required: true},
   title: {type: String, required: true},
-  description: {type, String, required: true},
+  description: {type: String, required: true},
   when:{type: Date, required: true},
   done:{type: Boolean, default: false},
   created:{type: Date, default: Date.now()},
