63b0b586f860bea41c52bec9a75f56b459cf16ec
Henrique Oliveira <henrique.fsa97@hotmail.com>
2020-07-17 16:16:31 -0300

    :sparkles: Crud

--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const TaskController = require('../controller/TaskController')
-const TaskValidation = require('../middlewares/TaskValidation')
+const TaskController = require('../controller/TaskController');
+const TaskValidation = require('../middlewares/TaskValidation');
+const MacValidation = require('../middlewares/MacValidation');
 
-router.post('/',TaskValidation ,TaskController.create);
+router.post('/',TaskValidation, TaskController.create);
+router.put('/:id',TaskValidation, TaskController.update);
+router.get('/:id', TaskController.show);
+
+router.get('/filter/all', MacValidation, TaskController.all);
+
 
 module.exports = router;
