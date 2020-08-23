bbdce36bf7ce39485526d96eab87ef591a73c0e6
Henrique Oliveira <henrique.fsa97@hotmail.com>
2020-08-22 21:32:47 -0300

    :hammer: mongoose update code

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 const url = 'mongodb://localhost:27017/itask';
-mongoose.connect(url, {useNewUrlParser: true});
+mongoose.connect(url, {useUnifiedTopology:true,useNewUrlParser: true});
 
 module.exports = mongoose;
