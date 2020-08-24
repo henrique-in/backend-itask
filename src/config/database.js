1d1533c1776dda718fe2bdb296255e8f62c6b02b
Henrique Oliveira <henrique.fsa97@hotmail.com>
2020-08-24 19:44:13 -0300

    add dotenv

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb+srv://itask:backend@itask.kdcwg.gcp.mongodb.net/itask;';
+require('dotenv').config();
+
+const url = process.env.MONGO_URL;
 
 mongoose.connect(url, {useUnifiedTopology:true,useNewUrlParser: true}).then(()=>{
   console.log('Connected')
